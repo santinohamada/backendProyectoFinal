ba7496db303425e95034ae6eb93bd3780e4f4810
Gustavo Gettar <gus.gettar@gmail.com>
2024-10-18 15:25:44 -0300

    agregue rutas de usuario + login

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
 const router = Router()
 
+usuarioRouter
+  .route("/")
+  .post([validacionUsuario], crearUsuario)
+  .get(listarUsuarios);
+usuarioRouter
+  .route("/login")
+  .post(login)
+  
+
 router.route("/usuarios")
 export default router
