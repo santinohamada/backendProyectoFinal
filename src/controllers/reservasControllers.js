e010103bb42f16c116f0ea91d9b727662b620cf3
Matias Salazar <matias39974593@gmail.com>
2024-10-22 22:01:40 -0300

    se agrego una funsion obtenerReserva para buscarla por id

--- src/controllers/reservasControllers.js
+++ src/controllers/reservasControllers.js
@@ -14,10 +14,13 @@ export const listarReservas = async(req, res)=>{
 export const obtenerReserva = async(req, res)=>{
     try {
         console.log(req.params.id)
+        const reservaBuscada = await Reservas.findById(req.params.id)
+        console.log(reservaBuscada)
+        res.status(200).json(reservaBuscada)
     } catch (error) {
         console.error(error);
-        res.status(404).json({
-            mensaje:"Ocurrio un error, no se pudo listar las reservas"
+        res.status(500).json({
+            mensaje:"Ocurrio un error, no se pudo obtener la reserva"
         })
     }
 }
