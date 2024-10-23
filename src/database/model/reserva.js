55748cf40e1753889a70d9bb530cacdf029d7200
SantinoHamada <hamadasantino@hotmail.com>
2024-10-23 20:53:45 -0300

    Se añadio el id de la habitacion a la reserva

--- src/database/model/reserva.js
+++ src/database/model/reserva.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const reservaSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  roomId: { type: mongoose.Schema.Types.ObjectId, required: true },
   roomNumber: { type: Number, required: true },
   checkIn: { type: Date, required: true },
   checkOut: { type: Date, required: true },
